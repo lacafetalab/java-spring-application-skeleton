e1c45eacffb42707c72e91eeec464efc1c070481
Jose Guillermo <jguillermo@outlook.com>
2020-02-12 19:35:48 -0500

    feat: add ddd core

--- apps/main/pe/lacafetalab/application/autentication/controller/health_check/HealthCheckGetController.java
+++ apps/main/pe/lacafetalab/application/autentication/controller/health_check/HealthCheckGetController.java
@@ -1,6 +1,5 @@
 package pe.lacafetalab.application.autentication.controller.health_check;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +8,9 @@ import java.util.HashMap;
 @RestController
 public class HealthCheckGetController {
     @GetMapping("health-check")
-    public HashMap<String,String> index(){
-        HashMap<String,String> status = new HashMap<>();
-        status.put("status","ok");
+    public HashMap<String, String> index() {
+        HashMap<String, String> status = new HashMap<>();
+        status.put("status", "ok");
         return status;
 
     }
