a7b80d5f8500ec24a831f0ce9c5dcc083794834e
Jose Guillermo <jguillermo@outlook.com>
2020-03-21 18:55:13 -0500

    refactor: unit testing

--- src/pao/test/pe/lacafetalab/pao/courses/application/create/CourseCreatorTest.java
+++ src/pao/test/pe/lacafetalab/pao/courses/application/create/CourseCreatorTest.java
@@ -1,17 +1,28 @@
 package pe.lacafetalab.pao.courses.application.create;
 
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pe.lacafetalab.pao.courses.domain.*;
 
 import static org.mockito.Mockito.*;
 
 class CourseCreatorTest {
+    private CourseCreator creator;
+    private CourseRepository repository;
+
+    @BeforeEach
+    protected void setUp() {
+        repository = mock(CourseRepository.class);
+        creator = new CourseCreator(repository);
+    }
+
+    protected void shouldHaveSaved(Course course){
+        verify(repository, atLeastOnce()).save(course);
+    }
 
     @Test
     void save_a_valid_course() {
-        CourseRepository repository = mock(CourseRepository.class);
-        CourseCreator creator = new CourseCreator(repository);
 
         CreateCourseRequest request = CreateCourseRequestMother.random();
 
@@ -19,6 +30,8 @@ class CourseCreatorTest {
 
         creator.create(request);
 
-        verify(repository, atLeastOnce()).save(course);
+        shouldHaveSaved(course);
+
+
     }
 }
